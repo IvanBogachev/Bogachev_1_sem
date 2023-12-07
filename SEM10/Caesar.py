ee30d82e1e0bed31768d42010e954b43032e22b6
IvanBogachev <bogachyov089@mail.ru>
2023-12-07 17:19:30 +0300

    да

--- SEM10/Caesar.py
+++ SEM10/Caesar.py
@@ -3,28 +3,23 @@ class Caesar:
 
     def __init__(self, key):
         self._encode = dict()
-        self._decode = dict()
         for i in range(len(self.alphabet)):
             letter = self.alphabet[i]
             encoded = self.alphabet[(i + key) % len(self.alphabet)]
-            decode = self.alphabet[(i - key) % len(self.alphabet)]
             self._encode[letter] = encoded
             self._encode[letter.upper()] = encoded.upper()
-            self._decode[letter] = encoded
-            self._decode[letter.upper()] = encoded.upper()
-
-        self._decode = {}
+        self._decode = {}  # TODO
 
     def encode(self, text):
         return ''.join([self._encode.get(char, char) for char in text])
 
-    def decode(self, line):
-        return ''.join([self._encode.get(char, char) for char in line])
+    def decode(self, text):
+        return ''.join([self._encode.get(char, char) for char in text])
 
 
 key = 19
 cipher = Caesar(key)
 line = input()
-while line != '.':
+while line:
     print(cipher.decode(line))
     line = input()
